fdb62741d0eb248cb88dba1b40f221b1bf979add
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2025-06-26 15:00:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const data = [{
+  Name: "Roshan Jaiswal",
+  Age: "23",
+  Education: "Btech",
+
+},
+{
+  Name: "Rajan Jaiswal",
+  Age: "19",
+  Education: "12",
+}]
+
+app.get('/', (req, res) => {
+  res.send('Hello, I AM Roshan jaiswal!');
+});
+
+app.get('/data', (req, res) => {
+  res.json(data);
+})
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}`);
+});
